f0411341202e36cc2f19a172367592c858d91920
acolletti21 <ashleymcolletti@gmail.com>
2017-03-05 14:11:21 -0600

    deletes neighborhood choices before saving

--- app/controllers/neighborhood_choices_controller.rb
+++ app/controllers/neighborhood_choices_controller.rb
@@ -11,23 +11,25 @@ class NeighborhoodChoicesController < ApplicationController
   end
 
   def update
-     @user = User.find(params[:id])
+    @user = User.find(params[:id])
 
-     neighborhood_ids = params[:neighborhood][:id]
-     neighborhood_ids.each do |neighborhood_id|
-       NeighborhoodChoice.find_or_create_by(
-                                            user_id: params[:user_id],
-                                            neighborhood_id: neighborhood_id
-                                            )
-       # if it exists, update as well. iterate through to let users deselect.
-     end
-
-    redirect_to "/users/#{@user.id}"
-
-     # @neighborhood_choices.each do |neighborhood_id|
-     #   NeighborhoodChoice.create( neighborhood_id: neighborhood_id, user_id: @user.id )
-       # if 
-       #logic to only add neighborhoods that haven't already been saved 
-     # end
+    @user.neighborhood_choices.destroy_all
+    neighborhood_ids = params[:neighborhood][:id]
+      if neighborhood_ids 
+        neighborhood_ids.each do |neighborhood_id|
+          if neighborhood_id != nil
+            NeighborhoodChoice.find_or_create_by(
+                                              user_id: params[:user_id],
+                                              neighborhood_id: neighborhood_id
+                                              )
+            
+            end
+          end
+        flash[:success] = "Successfully Updated Neighborhoods"
+        redirect_to "/users/#{@user.id}"
+      else
+      render :edit
+    end
   end
+
 end
