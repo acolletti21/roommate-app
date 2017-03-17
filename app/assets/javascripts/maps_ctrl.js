15fda736d2c3e025404a58780e9f8cb745e0281a
acolletti21 <ashleymcolletti@gmail.com>
2017-03-17 13:26:01 -0500

    adjusted theme, sort neighborhood list by name

--- app/assets/javascripts/maps_ctrl.js
+++ app/assets/javascripts/maps_ctrl.js
@@ -2,7 +2,7 @@
  "use strict";
  
  angular.module("app").controller("mapsCtrl", function($scope){
-  var names = ["Avondale", "Lakeview", "Roscoe Village", "Logan Square", "Bucktown", "Clyborn Corridor", "Lincoln Park", "Humboldt Park", "Wicker Park", "Goose Island", "Old Town", "Gold Coast", "Ukrainian Village", "Goose Island", "River West", "River North", "Streeterville", "West Town", "Little Italy", "West Loop", "Loop", "South Loop", "Pilsen", "Bridgeport", "Chinatown", "Bronzeville"];
+  var names = ["Avondale", "Lakeview", "Roscoe Village", "Logan Square", "Bucktown", "Clyborn Corridor", "Lincoln Park", "Humboldt Park", "Wicker Park", "Goose Island", "Old Town", "Gold Coast", "Ukrainian Village", "River West", "River North", "Streeterville", "West Town", "Little Italy", "West Loop", "Loop", "South Loop", "Pilsen", "Bridgeport", "Chinatown", "Bronzeville"];
   
      //add more models here
   window.scope = $scope;
--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -1,5 +1,5 @@
 class Neighborhood < ApplicationRecord
-  belongs_to :user
+  
   has_many :neighborhood_choices
   has_many :users, through: :neighborhood_choices
 
