5e520256d3eeb4fded31c559d2fd5dbe80ad1185
acolletti21 <ashleymcolletti@gmail.com>
2017-03-18 16:53:21 -0500

    new svg map now side by side w listings scroll div

--- app/assets/javascripts/maps_ctrl.js
+++ app/assets/javascripts/maps_ctrl.js
@@ -5,6 +5,13 @@
   var names = ["Avondale", "Lakeview", "Roscoe Village", "Logan Square", "Bucktown", "Clyborn Corridor", "Lincoln Park", "Humboldt Park", "Wicker Park", "Goose Island", "Old Town", "Gold Coast", "Ukrainian Village", "River West", "River North", "Streeterville", "West Town", "Little Italy", "West Loop", "Loop", "South Loop", "Pilsen", "Bridgeport", "Chinatown", "Bronzeville"];
   
      //add more models here
+
+  var paths = document.getElementsByTagName('path');
+  for (var i=0, length = paths.length; i < length; i++) {
+     paths[i].className = "newclass"; //this will go through all of them to change the class but i'll leave this here for reference..
+     //     var searchKey = paths[i]; < try something like that to make the clicked item's id th new search/sort term
+  }
+
   window.scope = $scope;
  });
 }());
